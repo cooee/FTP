1ad8d932829cd85ee46011423bd127d9af9186f0
Yucheng Mo <yucheng.mo@maxwit.com>
2012-04-18 21:57:44 +0800

    button desinger
    Signed-off-by: Yucheng Mo <yucheng.mo@maxwit.com>

--- Client/ftpClient/mainwindow.cpp
+++ Client/ftpClient/mainwindow.cpp
@@ -1,12 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "QFileDialog"
+#include "QSplitter"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     actionConnect();
+//    QHBoxLayout *labelLayout = new QHBoxLayout;
+//    labelLayout->addWidget(ui->mAbout);
+//    QHBoxLayout *rotateSliderLayout = new QHBoxLayout;
+//    rotateSliderLayout->addWidget(ui->mReconnect);
+//    QGridLayout *topLayout = new QGridLayout;
+//    topLayout->addWidget(ui->centralWidget);
+//    topLayout->addLayout(labelLayout, 0, 0);
+//    topLayout->addLayout(rotateSliderLayout,1, 1);
+//    setLayout(topLayout);
+    QSplitter *h1Splitter = new QSplitter;
+    QSplitter *h2Splitter = new QSplitter;
+    QPushButton * button = new QPushButton(this);
+    QPushButton * button1 = new QPushButton(this);
+    button->setText("sss");
+     button1->setText("sss");
+     QSplitter *vSplitter = new QSplitter;
+     vSplitter->setOrientation(Qt::Horizontal);
+     h1Splitter->addWidget(button);
+     h2Splitter->addWidget(button1);
+     vSplitter->addWidget(h1Splitter);
+     vSplitter->addWidget(h2Splitter);
+     ui->gridLayout_4->addWidget(vSplitter);
 
 }
 
